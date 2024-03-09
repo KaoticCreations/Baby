6095f7c208b89fd153fbf679ce95a463eeb569ec
KaoticCreations <161386134+KaoticCreations@users.noreply.github.com>
2024-03-08 21:03:39 -0500

    Update kscript.js

--- kscript.js
+++ kscript.js
@@ -6,14 +6,16 @@
 /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
 
 function openNav() {
-  document.getElementById("mySidebar").style.width = "80%"; 
-  document.getElementById("menuClose").style.marginLeft ="20%"; 
+  document.getElementById("mySidebar").style.width = "70%"; 
+  document.getElementById("menuClose").style.marginLeft ="0%"; 
   document.getElementById("menuClose").style.display = "block";
-  document.getElementById("menuClose").style.transition="0.5s";
+  
   document.getElementById("mySidebar").style.height = "auto";
   document.getElementById("openbtn").style.display = "none";
 
-  document.getElementById("kmain").style.marginLeft = "0px";      
+  document.getElementById("homeTop").style.marginright = "0px";
+
+  
        
   
 }
@@ -22,15 +24,9 @@ function openNav() {
 
 function closeNav() {
     document.getElementById("mySidebar").style.width = "0px"; 
-    document.getElementById("menuClose").style.marginLeft ="0%"; 
-  document.getElementById("menuClose").style.display = "none";  
+    document.getElementById("menuClose").style.marginLeft ="-10%"; 
+    //document.getElementById("menuClose").style.display = "none";  
     document.getElementById("openbtn").style.display = "block";
+    document.getElementById("kTainr").style.marginLeft = "0px";   
 
-
-    
-    document.getElementById("kmainSono").style.marginLeft = "0px";
-    document.getElementById("kHeadr").style.marginLeft = "0px";
-    document.getElementById("kHeadr").style.transition= ".3";
-    document.getElementById("kSidenav").style.marginLeft = "0px";
- 
 }
